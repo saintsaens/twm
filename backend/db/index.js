7ee391a690c44fcf41cc5dd4b4d52d66da497ead
saintsaens <saintsaens@users.noreply.github.com>
2024-10-15 11:28:50 +0200

    Cosmetics

--- backend/db/index.js
+++ backend/db/index.js
@@ -1,17 +1,17 @@
 import pg from 'pg'
 const { Pool } = pg
- 
+
 const pool = new Pool({
   database: 'witcher-ecommerce'
-})
+});
 
 pool.connect((err, client, release) => {
-    if (err) {
-      return console.error('Error acquiring client', err.stack);
-    }
-    console.log('Connected to PostgreSQL database successfully!');
-  });
- 
-export const query = (text, params, callback) => {
-    return pool.query(text, params, callback)
+  if (err) {
+    return console.error('Error acquiring client', err.stack);
   }
+  console.log('Connected to PostgreSQL database successfully!');
+});
+
+export const query = (text, params, callback) => {
+  return pool.query(text, params, callback)
+}
