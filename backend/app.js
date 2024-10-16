71ffba3189fa7d57cef3f4bec42c75462e23edb9
saintsaens <saintsaens@users.noreply.github.com>
2024-10-16 15:31:37 +0200

    Cosmetics

--- backend/app.js
+++ backend/app.js
@@ -1,17 +1,14 @@
 import express from "express";
-const app = express()
-const port = 3001
 import mountRoutes from "./routes/index.js";
+const app = express();
+const port = 3001;
 
 // Middleware to parse JSON request bodies
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-});
 
 mountRoutes(app);
 
 app.listen(port, () => {
-  console.log(`App listening on port ${port}`)
+  console.log(`Listening on port ${port}…`)
 });
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,11 @@
+import auth from "./auth.js"
 import items from './items.js'
 import orders from './orders.js'
 import sellers from './sellers.js'
 import users from './users.js'
  
 const mountRoutes = (app) => {
+  app.use('/', auth);
   app.use('/items', items);
   app.use('/orders', orders);
   app.use('/sellers', sellers);
