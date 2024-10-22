08c490c7237c633c383fd190d19c377b20a45dfd
saintsaens <saintsaens@users.noreply.github.com>
2024-10-22 22:30:36 +0200

    Add CSS

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,15 +7,19 @@ import Signin from './components/Signin';
 
 function App() {
   return (
-    <div>
+    <div className="app-container">
       <Router>
-        <h1>Buy stuff from The Witcher</h1>
-        <Navigation />
-        <Routes>
-          <Route path="/" element={<Items />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/signin" element={<Signin />} />
-        </Routes>
+        <header>
+          <h1>The Witcher Marketplace</h1>
+          <Navigation />
+        </header>
+        <main>
+          <Routes>
+            <Route path="/" element={<Items />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/signin" element={<Signin />} />
+          </Routes>
+        </main>
       </Router>
     </div>
   );
@@ -23,16 +27,16 @@ function App() {
 
 function Navigation() {
   return (
-    <nav>
+    <nav className="navigation">
       <ul>
         <li>
-          <Link to="/">Items</Link>
+          <Link to="/" className="nav-link">Items</Link>
         </li>
         <li>
-          <Link to="/signup">Sign up</Link>
+          <Link to="/signup" className="nav-link">Sign up</Link>
         </li>
         <li>
-          <Link to="/signin">Sign in</Link>
+          <Link to="/signin" className="nav-link">Sign in</Link>
         </li>
       </ul>
     </nav>
