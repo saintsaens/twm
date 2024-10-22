c0dafcc56995b52c7fd1d3c96a33a59301bd0e81
saintsaens <saintsaens@users.noreply.github.com>
2024-10-22 22:32:01 +0200

    Add a basic signin form

--- frontend/src/components/Signin.js
+++ frontend/src/components/Signin.js
@@ -1,7 +1,35 @@
 import React from 'react';
 
 function Signin() {
-  return <h2>Sign in</h2>;
+    return (
+        <div>
+            <h1>Sign in</h1>
+            <form action="/login/password" method="post">
+                <section>
+                    <label htmlFor="username">Username</label>
+                    <input
+                        id="username"
+                        name="username"
+                        type="text"
+                        autoComplete="username"
+                        required
+                        autoFocus
+                    />
+                </section>
+                <section>
+                    <label htmlFor="current-password">Password</label>
+                    <input
+                        id="current-password"
+                        name="password"
+                        type="password"
+                        autoComplete="current-password"
+                        required
+                    />
+                </section>
+                <button type="submit">Sign in</button>
+            </form>
+        </div>
+    );
 }
 
 export default Signin;
