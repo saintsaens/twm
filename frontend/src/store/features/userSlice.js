9f16edae062ffb764f7c47052f466ee56f22f709
saintsaens <saintsaens@users.noreply.github.com>
2024-10-25 20:45:07 +0200

    Retrieve user id

--- frontend/src/store/features/userSlice.js
+++ frontend/src/store/features/userSlice.js
@@ -1,37 +1,46 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 
 export const fetchUser = createAsyncThunk('user/fetchUser', async () => {
-  const response = await fetch('/api/user/profile', { credentials: 'include' });
-  if (response.ok) {
-    const data = await response.json();
-    return data.username;
-  }
-  throw new Error('Not logged in');
+    const response = await fetch('/api/user/profile', { credentials: 'include' });
+    if (response.ok) {
+        const data = await response.json();
+        return ({
+            userId: data.id,
+            username: data.username,
+        });
+    }
+    throw new Error('Not logged in');
 });
 
 const userSlice = createSlice({
-  name: 'user',
-  initialState: { username: '', status: 'idle', error: null },
-  reducers: {
-    clearUser: (state) => {
-        state.username = '';
-        state.status = 'idle';
+    name: 'user',
+    initialState: {
+        userId: '',
+        username: '',
+        status: 'idle',
+        error: null
     },
-  },
-  extraReducers: (builder) => {
-    builder
-      .addCase(fetchUser.pending, (state) => {
-        state.status = 'loading';
-      })
-      .addCase(fetchUser.fulfilled, (state, action) => {
-        state.status = 'succeeded';
-        state.username = action.payload;
-      })
-      .addCase(fetchUser.rejected, (state, action) => {
-        state.status = 'failed';
-        state.error = action.error.message;
-      });
-  },
+    reducers: {
+        clearUser: (state) => {
+            state.username = '';
+            state.status = 'idle';
+        },
+    },
+    extraReducers: (builder) => {
+        builder
+            .addCase(fetchUser.pending, (state) => {
+                state.status = 'loading';
+            })
+            .addCase(fetchUser.fulfilled, (state, action) => {
+                state.status = 'succeeded';
+                state.username = action.payload;
+                state.userId = action.payload;
+            })
+            .addCase(fetchUser.rejected, (state, action) => {
+                state.status = 'failed';
+                state.error = action.error.message;
+            });
+    },
 });
 
 export const { clearUser } = userSlice.actions;
