c42b3f9b8e38e99398af1279decccd668273b2b8
saintsaens <saintsaens@users.noreply.github.com>
2024-10-27 09:56:03 +0100

    Display date and total price in order page

--- frontend/src/components/Order.js
+++ frontend/src/components/Order.js
@@ -3,28 +3,39 @@ import { useSelector, useDispatch } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import { fetchOrder } from '../store/features/orderSlice';
 import { Link } from "react-router-dom";
+import { fetchOrders } from "../store/features/ordersSlice";
 
 const Order = () => {
-  const { id } = useParams();
-  const dispatch = useDispatch();
-  
-  const { order } = useSelector((state) => state.order);
+    const { id } = useParams();
+    const dispatch = useDispatch();
 
-  useEffect(() => {
-    if (id) {
-      dispatch(fetchOrder(id));
-    }
-  }, [dispatch, id]);
+    const { order } = useSelector((state) => state.order);
+    const { orders } = useSelector((state) => state.orders);
+    const { userId } = useSelector((state) => state.user.userId);
+    const currentOrder = orders.find((order) => order.id === Number(id));
+    const date = currentOrder ? new Date(currentOrder.created_at).toLocaleString() : "Date not available";
+    const totalPrice = currentOrder ? currentOrder.total_price : null;
 
 
-  return (
-    <div className="items-container">
+    useEffect(() => {
+        if (id) {
+            dispatch(fetchOrder(id));
+        }
+        if (userId) {
+            dispatch(fetchOrders(userId));
+        }
+    }, [dispatch, id, userId]);
+
+
+    return (
+        <div className="items-container">
             <div className="go-back-link-container">
                 <p>
                     <Link to="/orders">← All orders</Link>
                 </p>
             </div>
             <h2>Order {id}</h2>
+            <p>Paid on {date}</p>
             <table className="items-table">
                 <thead>
                     <tr>
@@ -34,7 +45,7 @@ const Order = () => {
                     </tr>
                 </thead>
                 <tbody>
-                {order.map(({ item_id, name, price, quantity }, index) => (
+                    {order.map(({ item_id, name, price, quantity }, index) => (
                         <tr key={item_id || index}>
                             <td>{name}</td>
                             <td>{price}</td>
@@ -42,9 +53,17 @@ const Order = () => {
                         </tr>
                     ))}
                 </tbody>
+                <tfoot>
+                    <tr>
+                        <td colSpan="2" style={{ textAlign: 'right', fontWeight: 'bold' }}>Total Price:</td>
+                        <td>{totalPrice}</td>
+                    </tr>
+                    <tr>
+                    </tr>
+                </tfoot>
             </table>
         </div>
-  );
+    );
 };
 
 export default Order;
