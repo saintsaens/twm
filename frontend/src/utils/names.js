55b5a593feceb70c951dc9d61cb14ab0cbb6edc3
saintsaens <saintsaens@users.noreply.github.com>
2024-10-27 09:09:18 +0100

    Add more nickname possibilities

--- frontend/src/utils/names.js
+++ frontend/src/utils/names.js
@@ -1,20 +1,29 @@
 const adjectives = [
-    "Resplendent", "Inimitable", "Iridescent", "Sinuous", "Ethereal", "Sonorous",
-    "Candescent", "Celestial", "Opalescent", "Enigmatic", "Effulgent", "Ephemeral",
-    "Phantasmal", "Majestic", "Eloquent", "Eccentric", "Radiant", "Luminous",
-    "Fabled", "Sublime", "Transcendent", "Elusive", "Exquisite", "Seraphic",
-    "Venerable", "Mythic", "Bewitching", "Arcane", "Mesmeric", "Timeless"
+    "resplendent", "inimitable", "iridescent", "sinuous", "ethereal", "sonorous",
+    "candescent", "celestial", "opalescent", "enigmatic", "effulgent", "ephemeral",
+    "phantasmal", "majestic", "eloquent", "eccentric", "radiant", "luminous",
+    "fabled", "sublime", "transcendent", "elusive", "exquisite", "seraphic",
+    "venerable", "mythic", "bewitching", "arcane", "mesmeric", "timeless",
+    "arcadian", "serene", "phantom", "silken", "radiant", "verdant", 
+    "mystic", "arcane", "wondrous", "glimmering", "whimsical", "fabled",
+    "arcadian", "somber", "luminous", "velvet", "ethereal", "translucent",
+    "diaphanous", "sylvan", "celestial", "enchanting", "nebulous", "ancient",
+    "melancholy", "otherworldly", "halcyon", "sacred", "nocturnal", "secretive"
 ];
 
 const names = [
-    "Markhor", "Pangolin", "Quokka", "Kakapo", "Fossa",
-    "Vaquita", "Binturong", "Dhole", "Civet", "Takin", "Caracal", "Axolotl",
-    "Genet", "Saiga", "Manul", "Bongo", "Quoll", "Ibex", "Kudu", "Ratel",
-    "Nyala", "Sifaka", "Dhole", "Oryx", "Kinkajou", "Margay", "Raccoon Dog", "Galago"
+    "markhor", "pangolin", "quokka", "kakapo", "fossa",
+    "vaquita", "binturong", "dhole", "civet", "takin", "caracal", "axolotl",
+    "genet", "saiga", "manul", "bongo", "quoll", "ibex", "kudu", "ratel",
+    "nyala", "sifaka", "dhole", "oryx", "kinkajou", "margay", "raccoon dog", "galago",
+    "okapi", "ocelot", "dugong", "tapir", "taruca", "uromastyx", "zebu", "kinkajou", 
+    "chital", "tarsier", "kodiak", "eagle", "pronghorn", "muskrat", "albatross", 
+    "mongoose", "meerkat", "macaw", "numbat", "loris", "coati", "weasel", "lemur", 
+    "oryx", "echidna", "goral", "galah", "bonobo", "cassowary", "kagu"
 ];
 
 export function generateName() {
     const adjective = adjectives[Math.floor(Math.random() * adjectives.length)];
-    const animal = names[Math.floor(Math.random() * names.length)];
-    return `${adjective} ${animal}`;
+    const animal = names[Math.floor(Math.random() * names.length)].replace(" ", "-");
+    return `${adjective}-${animal}`;
 }
