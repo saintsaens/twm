79e5712b3f647ce1b7a7f59c473c9187c8088f1f
saintsaens <saintsaens@users.noreply.github.com>
2024-11-07 10:11:10 +0100

    Check authentication at page load

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,7 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
 import { BrowserRouter as Router } from "react-router-dom";
+import { fetchUser } from "./store/features/userSlice";
 import '@gouvfr/dsfr/dist/dsfr.min.css';
 
 import Navigation from './components/Navigation';
@@ -7,6 +9,12 @@ import AppRoutes from './components/AppRoutes';
 import Title from "./components/Title";
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchUser());
+  }, [dispatch]);
+
   return (
     <Router>
       <header>
