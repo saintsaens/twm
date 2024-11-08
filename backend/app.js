d423a1db41544898f132bfd1ab10b68ba349788c
saintsaens <saintsaens@users.noreply.github.com>
2024-11-08 17:36:32 +0100

    Add settings to cookie for cross origin login

--- backend/app.js
+++ backend/app.js
@@ -31,7 +31,12 @@ app.use(session({
   secret: process.env.SESSION_SECRET,
   resave: false,
   saveUninitialized: false,
-  cookie: { secure: process.env.NODE_ENV === "production" }
+  cookie: {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "render",
+    sameSite: process.env.NODE_ENV === 'render' ? 'None' : 'Lax',
+    maxAge: 24 * 60 * 60 * 1000
+  }
 }));
 app.use(passport.initialize());
 app.use(passport.authenticate('session'));
