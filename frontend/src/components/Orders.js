cb133fc69ca891a3d353a2882b3e8c5bf3254ed0
saintsaens <saintsaens@users.noreply.github.com>
2024-11-29 10:33:18 +0100

    Display better empty state when there are no orders

--- frontend/src/components/Orders.js
+++ frontend/src/components/Orders.js
@@ -1,12 +1,12 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchOrders } from '../store/features/ordersSlice';
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 function Orders() {
     const dispatch = useDispatch();
     const navigate = useNavigate();
-    const { orders } = useSelector((state) => state.orders);
+    const { orders, loading, error } = useSelector((state) => state.orders);
     const { userId } = useSelector((state) => state.user);
 
     useEffect(() => {
@@ -21,50 +21,68 @@ function Orders() {
 
     return (
         <>
-            <h1>
-                Your orders
-            </h1>
-            <div className="fr-table" id="table-md-component">
-                <div className="fr-table__wrapper">
-                    <div className="fr-table__container">
-                        <div className="fr-table__content">
-                            <table id="table-md">
-                                <thead>
-                                    <tr>
-                                        <th scope="col">ID</th>
-                                        <th scope="col">Nickname</th>
-                                        <th scope="col">Date & time</th>
-                                        <th scope="col">Paid</th>
-                                        <th scope="col">Actions</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    {orders.map(({ id, created_at, total_price, nickname }) => (
-                                        <tr
-                                            key={id}
-                                            data-row-key={id}
-                                            className="fr-table__row--clickable"
-                                        >
-                                            <td>{id}</td>
-                                            <td>{nickname}</td>
-                                            <td>{new Date(created_at).toLocaleString()}</td>
-                                            <td>{total_price}</td>
-                                            <td>
-                                                <button
-                                                    className="fr-btn fr-btn--secondary fr-btn--sm"
-                                                    onClick={() => handleView(id)}
-                                                >
-                                                    View
-                                                </button>
-                                            </td>
-                                        </tr>
-                                    ))}
-                                </tbody>
-                            </table>
-                        </div>
+            <h1>Your orders</h1>
+            <Link to="/" className="fr-link">← Go back</Link>
+
+            {
+                loading ? (
+                    <div className="fr-empty-state">
+                        <p>Loading…</p>
                     </div>
-                </div>
-            </div>
+                ) : error ? (
+                    <div className="fr-empty-state">
+                        <p>Error loading orders! 😬</p>
+                    </div>
+                ) : orders.length === 0 ? (
+                    <div className="fr-empty-state">
+                        <p>No orders.</p>
+                        <Link to="/" className="fr-link">← Go back</Link>
+                    </div>
+                ) : (
+                    <>
+                        <div className="fr-table" id="table-md-component">
+                            <div className="fr-table__wrapper">
+                                <div className="fr-table__container">
+                                    <div className="fr-table__content">
+                                        <table id="table-md">
+                                            <thead>
+                                                <tr>
+                                                    <th scope="col">ID</th>
+                                                    <th scope="col">Nickname</th>
+                                                    <th scope="col">Date & time</th>
+                                                    <th scope="col">Paid</th>
+                                                    <th scope="col">Actions</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                {orders.map(({ id, created_at, total_price, nickname }) => (
+                                                    <tr
+                                                        key={id}
+                                                        data-row-key={id}
+                                                        className="fr-table__row--clickable"
+                                                    >
+                                                        <td>{id}</td>
+                                                        <td>{nickname}</td>
+                                                        <td>{new Date(created_at).toLocaleString()}</td>
+                                                        <td>{total_price}</td>
+                                                        <td>
+                                                            <button
+                                                                className="fr-btn fr-btn--secondary fr-btn--sm"
+                                                                onClick={() => handleView(id)}
+                                                            >
+                                                                View
+                                                            </button>
+                                                        </td>
+                                                    </tr>
+                                                ))}
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </>
+                )}
         </>
     );
 }
