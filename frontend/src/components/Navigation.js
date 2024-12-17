78c1d290bbc10a12fbfd7ba316ecef7bd6a47638
saintsaens <saintsaens@users.noreply.github.com>
2024-12-17 12:21:26 +0100

    Cosmetics

--- frontend/src/components/Navigation.js
+++ frontend/src/components/Navigation.js
@@ -23,7 +23,7 @@ function Navigation() {
   return (
     <nav className="fr-header__tools-links">
       <ul className="fr-btns-group">
-      {navLinks.map(link => (
+        {navLinks.map(link => (
           <li key={link.path}>
             <Link to={link.path} className={link.icon}>{link.label}</Link>
           </li>
