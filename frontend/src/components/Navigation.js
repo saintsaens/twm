4e6acd50df662e81739253cdda47d267ab77eeea
saintsaens <saintsaens@users.noreply.github.com>
2024-12-17 12:21:12 +0100

    Make navLinks more readable

--- frontend/src/components/Navigation.js
+++ frontend/src/components/Navigation.js
@@ -6,13 +6,19 @@ function Navigation() {
   const { username } = useSelector((state) => state.user);
   const { totalItems } = useSelector((state) => state.cart);
 
-  const navLinks = [
-    ...(username ? [] : [{ path: '/signin', label: 'Sign in', icon: "fr-btn fr-icon-lock-line" }]),
-    ...(username ? [] : [{ path: '/signup', label: 'Sign up', icon: "fr-btn fr-icon-account-line" }]),
-    ...(username ? [{ path: '/cart', label: `Cart (${totalItems})`, icon: "fr-btn fr-icon-shopping-cart-2-line" }] : []),
-    ...(username ? [{ path: '/orders', label: 'Your orders', icon: "fr-btn fr-icon-money-euro-circle-line" }] : []),
-    ...(username ? [{ path: '/logout', label: 'Log out', icon: "fr-btn fr-icon-logout-box-r-line" }] : []),
-  ];
+  const navLinks = [];
+  if (!username) {
+    navLinks.push(
+      { path: '/signin', label: 'Sign in', icon: "fr-btn fr-icon-lock-line" },
+      { path: '/signup', label: 'Sign up', icon: "fr-btn fr-icon-account-line" }
+    );
+  } else {
+    navLinks.push(
+      { path: '/cart', label: `Cart (${totalItems})`, icon: "fr-btn fr-icon-shopping-cart-2-line" },
+      { path: '/orders', label: 'Your orders', icon: "fr-btn fr-icon-money-euro-circle-line" },
+      { path: '/logout', label: 'Log out', icon: "fr-btn fr-icon-logout-box-r-line" }
+    );
+  }
 
   return (
     <nav className="fr-header__tools-links">
